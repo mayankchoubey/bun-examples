b1a723fa65cc6aa969bdf8d5ae104a7d7d8cfdac
Mayank Choubey <mayank.choubey@ushur.com>
2022-10-09 18:33:16 -0700

    .

---
+++ environment_variables.js
@@ -0,0 +1,10 @@
+console.log('Total envs:', Object.keys(Bun.env).length);
+console.log('All envs:', Bun.env);
+console.log('OS temporary directory:', Bun.env.TMPDIR);
+console.log('User name:', Bun.env.LOGNAME);
+
+
+// Total envs: 36
+// All envs: { NVM_INC: ..... <content-too-long>
+// OS temporary directory: /var/folders/hx/0f6tcxq52f396vfq0c8xfszc0000gn/T/
+// User name: mayankc
---
+++ process_env.js
@@ -0,0 +1,10 @@
+console.log('Total envs:', Object.keys(process.env).length);
+console.log('All envs:', process.env);
+console.log('OS temporary directory:', process.env.TMPDIR);
+console.log('User name:', process.env.LOGNAME);
+
+
+// Total envs: 36
+// All envs: { NVM_INC: ..... <content-too-long>
+// OS temporary directory: /var/folders/hx/0f6tcxq52f396vfq0c8xfszc0000gn/T/
+// User name: mayankc
