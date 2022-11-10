e8f50bccecebcd534a336414280be43c543ad2db
Mayank Choubey <mayank.choubey@ushur.com>
2022-11-09 22:26:04 -0800

    .

--- bun_native_file_server.js
+++ bun_native_file_server.js
@@ -1,12 +1,9 @@
-const { stat } = require("fs").promises;
-
 const BASE_PATH = "./public";
 
 Bun.serve({
   port: 3000,
   async fetch(req) {
     const filePath = BASE_PATH + new URL(req.url).pathname;
-    const fileData = await stat(filePath);
     const file = Bun.file(filePath);
     return new Response(file);
   },
